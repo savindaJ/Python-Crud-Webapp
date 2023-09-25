60ff70867751bd45b0912465b51c0765a0961c3c
SavindaJayasekara <thantrige32@gmail.com>
2023-09-25 21:21:46 +0530

    add bootstrap and backend

--- FlaskCrudApplication/App.py
+++ FlaskCrudApplication/App.py
@@ -1,29 +1,64 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
+app.secret_key = 'many random bytes'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '80221474'
-app.config['MYSQL_DB'] = 'webApplication'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'crud'
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def Index():
+    # cur = mysql.connection.cursor()
+    # cur.execute("SELECT  * FROM students")
+    # data = cur.fetchall()
+    # cur.close()
+
+    return render_template('index2.html')
 
 
 @app.route('/insert', methods=['POST'])
 def insert():
     if request.method == "POST":
+        flash("Data Inserted Successfully")
         name = request.form['name']
-        cusId = request.form['id']
-        gmail = request.form['gmail']
+        email = request.form['email']
         phone = request.form['phone']
+        print(email, name, phone)
+        # # cur = mysql.connection.cursor()
+        # cur.execute("INSERT INTO students (name, email, phone) VALUES (%s, %s, %s)", (name, email, phone))
+        # mysql.connection.commit()
+        return redirect(url_for('Index'))
 
-        print(name, cusId)
 
-        return redirect(url_for('index'))
+@app.route('/delete/<string:id_data>', methods=['GET'])
+def delete(id_data):
+    flash("Record Has Been Deleted Successfully")
+    # cur = mysql.connection.cursor()
+    # cur.execute("DELETE FROM students WHERE id=%s", (id_data,))
+    # mysql.connection.commit()
+    return redirect(url_for('Index'))
+
+
+@app.route('/update', methods=['POST', 'GET'])
+def update():
+    if request.method == 'POST':
+        id_data = request.form['id']
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        print(email)
+        # cur = mysql.connection.cursor()
+        # cur.execute("""
+        #        UPDATE students
+        #        SET name=%s, email=%s, phone=%s
+        #        WHERE id=%s
+        #     """, (name, email, phone, id_data))
+        # flash("Data Updated Successfully")
+        # mysql.connection.commit()
+        return redirect(url_for('Index'))
 
 
 if __name__ == "__main__":
