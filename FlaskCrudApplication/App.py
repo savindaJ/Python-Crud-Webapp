7753f392726f7850f2253f9fb3b2d854ba4663ad
SavindaJayasekara <thantrige32@gmail.com>
2023-09-25 16:36:19 +0530

    add pop up

--- FlaskCrudApplication/App.py
+++ FlaskCrudApplication/App.py
@@ -1,11 +1,29 @@
-from flask import Flask ,render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '80221474'
+app.config['MYSQL_DB'] = 'webApplication'
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/insert', methods=['POST'])
+def insert():
+    if request.method == "POST":
+        name = request.form['name']
+        cusId = request.form['id']
+        gmail = request.form['gmail']
+        phone = request.form['phone']
+
+        print(name, cusId)
+
+        return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
